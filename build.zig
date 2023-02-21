ac58b0f34a721b87d78b233fbdd6f3515db88f80
Calla <32691832+BanchouBoo@users.noreply.github.com>
2023-02-21 17:21:31 -0500

    Add build.zig for the zig package manager to use

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    b.addModule(.{
+        .name = "accord",
+        .source_file = std.Build.FileSource.relative("accord.zig"),
+        .dependencies = &[_]std.Build.ModuleDependency{},
+    });
+
+    const main_tests = b.addTest(.{
+        .root_source_file = std.Build.FileSource.relative("accord.zig"),
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+
+    const test_step = b.step("test", "Run tests for accord");
+    test_step.dependOn(&main_tests.step);
+}
